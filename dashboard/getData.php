e0fe6983657b44c0b7538c4a380d8f5164507957
Nanx Shaw <nanxshaw@gmail.com>
2024-07-01 16:17:33 +0700

    bug fix

--- dashboard/getData.php
+++ dashboard/getData.php
@@ -3,8 +3,8 @@ include '../koneksi.php';
 
 $query_user = mysqli_query($koneksi, "SELECT COUNT(*) AS total_user FROM tb_user") or die(mysqli_error($koneksi));
 $query_barang = mysqli_query($koneksi, "SELECT COUNT(*) AS total_barang FROM tb_barang") or die(mysqli_error($koneksi));
-$query_masuk = mysqli_query($koneksi, "SELECT COUNT(*) AS total_masuk FROM tb_masuk") or die(mysqli_error($koneksi));
-$query_keluar = mysqli_query($koneksi, "SELECT COUNT(*) AS total_keluar FROM tb_keluar") or die(mysqli_error($koneksi));
+$query_masuk = mysqli_query($koneksi, "SELECT SUM(jml_masuk) AS total_masuk FROM tb_masuk") or die(mysqli_error($koneksi));
+$query_keluar = mysqli_query($koneksi, "SELECT SUM(jml_keluar) AS total_keluar FROM tb_keluar") or die(mysqli_error($koneksi));
 $barang_masuk = mysqli_query($koneksi, "SELECT b.nama_brg, COALESCE(SUM(a.jml_masuk),0) AS total_masuk FROM tb_masuk a RIGHT JOIN tb_barang b ON a.barang_id = b.id_brg GROUP BY b.id_brg") or die(mysqli_error($koneksi));
 $barang_keluar = mysqli_query($koneksi, "SELECT b.nama_brg, COALESCE(SUM(a.jml_keluar),0) AS total_keluar FROM tb_keluar a RIGHT JOIN tb_barang b ON a.barang_id = b.id_brg GROUP BY b.id_brg") or die(mysqli_error($koneksi));
 
